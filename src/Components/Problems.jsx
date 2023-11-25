05c415f21b6dfb76e698cb3990335f619a0fc287
Mohammad hossein <Mhakbari230110@gmail.com>
2023-11-25 16:15:40 +0330

    creating crousal

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Footer from "../Components/Footer";
 import Navbar from "./Navbar";
 import Carousel from "../Components/carousel";
+import Filter from "./Filter";
 
 const Problems = () => {
   return (
     <div className="">
       <Navbar />
       <Carousel />
-      <Footer />
+      <Filter />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@ import card from "../Image/me.jpg";
 import logo from "../Image/main_logo.png";
 import Problems from "../Components/Problems";
 import carousel_problem from "../Image/Problems/markus-winkler-aYPtEknQmXE-unsplash.jpg";
+import filter from "../Image/Problems/geojango-maps-Z8UgB80_46w-unsplash.jpg";
 export const data = [
   {
     id: "mohammad",
@@ -14,5 +15,9 @@ export const data = [
   {
     id: "cp1",
     image: carousel_problem,
+  },
+  {
+    id: "cp2",
+    image: filter,
   },
 ];
