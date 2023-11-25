d0f10a35c48e2f84abe182c731cb0fdd4449b4e4
Mohammad Mojtaba Asadi <m_asadibakloee82@comp.iust.ac.ir>
2023-11-25 19:00:56 +0330

    adding card additionaly

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,7 @@
 import card from "../Image/me.jpg";
 import logo from "../Image/29722995_3-removebg-preview.png";
+import card_image from "../Image/card-top.jpg";
+import library from "../Image/Problems/declan-sun-aon-6I6Y6uQ-unsplash.jpg"
 
 export const data = [
   {
@@ -10,4 +12,12 @@ export const data = [
     id: "mohammadsss",
     image: logo,
   },
+  {
+    id:"ss",
+    image:card_image
+  },
+  {
+    id:"library",
+    image:library
+  }
 ];
