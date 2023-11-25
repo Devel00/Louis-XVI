d22c83b40bd413dc33eae93fa3c176c62373c69e
Mohammad hossein <Mhakbari230110@gmail.com>
2023-11-25 12:00:56 +0330

    first carousel

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../Components/Footer";
 import Navbar from "./Navbar";
+import Carousel from "../Components/carousel";
 
 const Problems = () => {
   return (
-    <div>
+    <div className="">
       <Navbar />
+      <Carousel />
       <Footer />
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import card from "../Image/me.jpg";
 import logo from "../Image/main_logo.png";
-
+import Problems from "../Components/Problems";
+import carousel_problem from "../Image/Problems/markus-winkler-aYPtEknQmXE-unsplash.jpg";
 export const data = [
   {
     id: "mohammad",
@@ -9,5 +10,9 @@ export const data = [
   {
     id: "mohammadsss",
     image: logo,
+  },
+  {
+    id: "cp1",
+    image: carousel_problem,
   },
 ];
