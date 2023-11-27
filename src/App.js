a1529d0162c03eae3e2c70496c1c0c928f1a7bfb
Mohammad Mojtaba Asadi <m_asadibakloee82@comp.iust.ac.ir>
2023-11-27 10:56:11 +0330

    manage_problem_without_footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 //import Navbar from "./Components/Navbar";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
-import Card from "./Components/Card"
+import Card from "./Components/Card";
+import Manager from "./Components/manage_problems";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Problems from "./Components/Problems";
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" exact element={<Problems />} />
           <Route path="/Login" exact element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
+          <Route path="/Manager" element={<Manager />} />
         </Routes>
       </BrowserRouter>
     </div>
