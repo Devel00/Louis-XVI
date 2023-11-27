3e38a95b7ac094564b4b6cdf86c9bec23275a9d3
Mohammad hossein <Mhakbari230110@gmail.com>
2023-11-27 17:32:29 +0330

    post request for login page

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -3,6 +3,8 @@ import Footer from "../Components/Footer";
 import Navbar from "./Navbar";
 import Carousel from "../Components/carousel";
 import Filter from "./Filter";
+import Card from "./Card";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 
 const Problems = () => {
   return (
@@ -10,6 +12,22 @@ const Problems = () => {
       <Navbar />
       <Carousel />
       <Filter />
+      <div className=" pt-6 flex justify-start gap-4 items-center">
+        <span className=" text-accent-200  text-[20px] pr-4">پرطرفدارها</span>
+        <div className=" border-b-2 border-bg-300/40 w-[80%]"></div>
+        <div className=" flex justify-center items-center">
+          <span className=" text-accent-200 pr-6">مشاهده همه</span>
+          <MdKeyboardArrowLeft size={25} />
+        </div>
+      </div>
+      <div className="">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       {/* <Footer /> */}
     </div>
   );
