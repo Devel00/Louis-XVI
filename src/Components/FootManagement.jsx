b0831e41ee4ce590b9742846e80a84f5f16a1cb2
Mostafa Meshkini <thirdwalnut@gmail.com>
2024-01-05 23:46:15 +0330

    Quick edit

--- src/Components/FootManagement.jsx
+++ src/Components/FootManagement.jsx
@@ -23,12 +23,10 @@ const Feet = () => {
       }
     };
     ShowFeet();
-    //   ShowProblems();
   }, []);
   console.log(feet);
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
