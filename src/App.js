93dd7f22b144cd6c3fd10cec4ae2472340d4e628
Mohammad Mojtaba Asadi <m_asadibakloee82@comp.iust.ac.ir>
2024-01-18 22:28:29 +0330

    completing idea crude

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import EditUser from "./Components/User/CRUDUser";
 import EditIdea from "./Components/Idea/EditIdea";
 import IdeaManager from "./Components/Idea/IdeaManger";
 import EditIdea from "./Components/EditIdea";
+import EditIdea from "./Components/EditIdea";
 function App() {
   return (
     <div className="App">
